7f707447288a7293d55d539d91a8cf37f5e7bc25
varu <g.varesh@gmail.com>
2013-10-22 01:38:23 +0530

    Update a.rb

--- git/a.rb
+++ git/a.rb
@@ -1,20 +1,17 @@
 a="978014300723"
 b=a.to_s.split('')
 e1=[]
-o1=[]
-sum = 0
+
 b.each_with_index do |i,k| 
 
-   until k.even?
-      e=i.to_i*3
-      e1<<e
-end
-    until k.odd?
-          o= i.to_i
-           o1<<o
-     end
-     
-     end
-e1.each { |a| sum+=a }
-
-
+ if k.even?
+     e1 << i.to_i
+ else
+     e1 << i.to_i * 3
+  end
+end 
+sum = 0 
+    e1.each {|i| sum += i} 
+    m = sum%10
+    l=10-m
+   puts a+l.to_s
